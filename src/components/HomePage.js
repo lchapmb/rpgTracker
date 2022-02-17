0c7585b0e6a0e23012d9f7955a1324a8ed0d228a
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-02-17 09:44:00 +0000

    styling changes, adds palette variables to root

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import TopNav from "./navs/TopNav";
 
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
 export default function HomePage() {
   return (
     <>
       <TopNav />
+      <Container className="homeContainer" maxWidth="lg" gutterTop>
+        <Box className="homeBox" sx={{ height: "50vh" }}>
+          <Typography variant="h1" component="div" gutterBottom align="center">
+            RPG Tracker
+          </Typography>
+        </Box>
+      </Container>
     </>
   );
 }
