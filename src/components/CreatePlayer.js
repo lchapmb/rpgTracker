36a7f0909eb50193542bc99f0faaaefc15269f95
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-02-17 12:29:46 +0000

    creates CreatePlayer page

---
+++ src/components/CreatePlayer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TopNav from "./navs/TopNav";
+
+export default function CreatePlayer() {
+  return (
+    <>
+      <TopNav />
+      <div>Create Player</div>
+    </>
+  );
+}
