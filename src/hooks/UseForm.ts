d3261051e8dd5d9765cfa8684a669a7d71f026ca
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-04 09:36:00 +0000

    removes console log

--- src/hooks/UseForm.ts
+++ src/hooks/UseForm.ts
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-import { SelectChangeEvent } from "@mui/material/Select";
 
 export default function UseForm(callback: any, initialState = {}) {
   const [values, setValues] = useState(initialState);
 
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [event.target.name]: event.target.value });
-    console.log(values);
   };
 
   const onSubmit = async (event: React.MouseEvent) => {
