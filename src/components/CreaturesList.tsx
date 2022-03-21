558fda3dae358e8fdd7b444cd1292e0aff0a3550
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-21 15:17:48 +0000

    removes max width on list

--- src/components/CreaturesList.tsx
+++ src/components/CreaturesList.tsx
@@ -23,7 +23,6 @@ export default function CreaturesList() {
         <List
           sx={{
             width: "100%",
-            maxWidth: 360,
             bgcolor: "background.paper",
           }}
         >
