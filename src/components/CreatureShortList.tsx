fdc066e9977acc9f6558ed818f4ebe9cef7ace87
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-21 14:57:43 +0000

    adds player names list to create player page

--- src/components/CreatureShortList.tsx
+++ src/components/CreatureShortList.tsx
@@ -1,8 +1,43 @@
+// imports from MUI
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+} from "@mui/material";
+import { teal } from "@mui/material/colors";
+import PetsIcon from "@mui/icons-material/Pets";
+
 // import context
 import { useCreaturesContext } from "../views/App";
 
-const { creaturesArr, setCreaturesArr } = useCreaturesContext();
-
 export default function CreatureShortList() {
-  return <div>CreatureShortList</div>;
+  const { creaturesArr } = useCreaturesContext();
+
+  return (
+    <List
+      sx={{
+        width: "100%",
+        maxWidth: 360,
+        bgcolor: "background.paper",
+      }}
+    >
+      {creaturesArr.map((creature) => (
+        <ListItem key={creature.id} alignItems="center">
+          <ListItemAvatar>
+            <Avatar sx={{ bgcolor: teal[900] }}>
+              <PetsIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText>
+            <Typography variant="subtitle1" component="div">
+              {creature.name}
+            </Typography>
+          </ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
