c519a30f27ddf2422c611ec4e5ca261a9894b10f
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-21 14:12:53 +0000

    creates player shortlist component

---
+++ src/components/CreatureShortList.tsx
@@ -0,0 +1,8 @@
+// import context
+import { useCreaturesContext } from "../views/App";
+
+const { creaturesArr, setCreaturesArr } = useCreaturesContext();
+
+export default function CreatureShortList() {
+  return <div>CreatureShortList</div>;
+}
