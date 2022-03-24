5900e85f045eb8f5729277888346ea3d359c4aa6
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-03-24 13:26:49 +0000

    creates creaturesforcombat file

--- src/components/CombatPage.tsx
+++ src/components/CombatPage.tsx
@@ -1,16 +1,24 @@
 // import components
 import TopNav from "./navs/TopNav";
 import BottomNav from "./navs/BottomNav";
+import CreaturesForCombat from "./CreaturesForCombat";
 
 // imports from MUI
-import { Container } from "@mui/material";
+import { Container, Typography, Box } from "@mui/material";
 
 export default function CombatPage() {
   return (
     <>
       <TopNav />
       <Container>
-        <div>CombatPage</div>
+        <Box className="creatureTitleBox" sx={{ height: "auto" }}>
+          <Typography variant="h3" component="div" gutterBottom align="center">
+            Combat
+          </Typography>
+        </Box>
+        <Box className="creatureListContainer" maxWidth="lg" mt={1}>
+          <CreaturesForCombat />
+        </Box>
         <BottomNav />
       </Container>
     </>
---
+++ src/components/CreaturesForCombat.tsx
@@ -0,0 +1,3 @@
+export default function CreaturesForCombat() {
+  return <div>CreaturesForCombat</div>;
+}
