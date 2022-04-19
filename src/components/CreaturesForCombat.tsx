46ba409df60f3ad3b6f0fffe89780a58011734eb
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-04-19 12:31:57 +0100

    remove unused improts, commments out some vars, adds sort for intArr

--- src/components/CreaturesForCombat.tsx
+++ src/components/CreaturesForCombat.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 // import context
 import { useInitiativeContext } from "../views/App";
@@ -10,24 +10,36 @@ import {
   ListItemAvatar,
   Avatar,
   ListItemText,
-  Button,
 } from "@mui/material";
 import { teal } from "@mui/material/colors";
 import PetsIcon from "@mui/icons-material/Pets";
 
 // import interface
-import Character from "../models/CharacterModel";
+// import CombatantInterface from "../interface/CombatantInterface";
 
 export default function CreaturesForCombat() {
   // define initial state
-  const initialState = {
-    selectedCreature: new Character("", 0, 0),
-  };
+  // const initialState = {
+  //   initiativeArr: Array<CombatantInterface>(),
+  // };
 
   // set state
-  const [values, setValues] = useState(initialState);
+  // const [values, setValues] = useState(initialState);
 
   const { initiativeArr } = useInitiativeContext();
+
+  const initiativeSort = (
+    a: { initiative: number },
+    b: { initiative: number }
+  ) => {
+    if (a.initiative < b.initiative) return 1;
+    else if (a.initiative > b.initiative) return -1;
+    else return 0;
+  };
+
+  let sortedCombatants = initiativeArr.sort(initiativeSort);
+
+  // setValues(sortedCombatants);
 
   return (
     <List
@@ -36,7 +48,7 @@ export default function CreaturesForCombat() {
         bgcolor: "background.paper",
       }}
     >
-      {initiativeArr.map((creature) => (
+      {sortedCombatants.map((creature) => (
         <ListItem key={creature.id} alignItems="flex-start">
           <ListItemAvatar>
             <Avatar sx={{ bgcolor: teal[900] }}>
